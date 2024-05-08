a231ded0ff99fd2beebdfd941be631567f2f7ea5
Vikas Kumar <vikaskumar9891452674@gmail.com>
2024-05-08 11:31:01 +0530

    Some bug fixes and changed the way of showing loader and improved data filter

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -4,8 +4,6 @@ import { useEffect, useMemo, useState } from "react";
 import Card from "../card/Card";
 
 const Search = () => {
-
-  // all the state variables
   const [data, setApiData] = useState([]);
   const [experiences, setExperiences] = useState("");
   const [remotes, setRemotes] = useState([]);
@@ -14,85 +12,121 @@ const Search = () => {
   const [company, setCompany] = useState("");
   const [offset, setOffset] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [preLength, setPrevLength] = useState(0);
   const [limit, setLimit] = useState(10);
 
-
-
-      /**
-   * Fetches data from the specified API endpoint and updates the state with the fetched data.
-   *
-   * @return {Promise<void>} - A promise that resolves when the data is fetched and the state is updated.
-   */
-      const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(
-          "https://api.weekday.technology/adhoc/getSampleJdJSON",
-          {
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify({
-              limit: limit,
-              offset: offset,
-            }),
-          }
-        );
-  
-        const data = await response.json();
-  
-        // Filter out entries where any property is null
-        const filteredData = data.jdList.filter(
-          (item) => !Object.values(item).includes(null)
-        );
-  
-        setApiData((prevData) => [...prevData, ...filteredData]);
-        setLoading(false)
-      } catch (e) {
-        console.log("Error on Fetching data", e);
-      }
-    };
-
-        /**
+    /**
      * Handles the infinite scroll behavior by incrementing the offset state when the user scrolls to the bottom of the page.
      *
      * @return {void} This function does not return anything.
      */
   const infiniteScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop+10 >= document.documentElement.scrollHeight) {
+        if (window.innerHeight + document.documentElement.scrollTop+10 >= document.documentElement.scrollHeight) {
 
-        setOffset(prev => prev+1)
+            setOffset(prev => prev+1)
+        }
     }
-  }
 
-   // Adding scroll event and remove on unmount
-  useEffect(() => {
-    console.log('scroll');
+
+  /**
+   * Filters the data based on the user's search criteria.
+   *
+   * @return {array} The filtered data.
+  */
+  const filteredData = useMemo(() => {
+    return data.filter(item => {
+      return (
+        (!experiences || item.minExp == experiences) && // Pass if experiences is empty or matches
+        (!remotes.length || remotes.includes(item.location)) && // Pass if remotes array is empty or location matches
+        (!basePays || (item.minJdSalary >= Number(basePays) && item.minJdSalary < Number(basePays)+10)) && // Pass if basePays is empty or matches
+        (!roles.length || roles.some(role => item.jobRole && item.jobRole.includes(role))) && // Pass if roles array is empty or any role matches
+        (!company || item.companyName.toLowerCase().startsWith(company)) // Pass if company is empty or matches
+      );
+    });
+  }, [data, experiences, remotes, basePays, roles, company]);
+
+      /**
+     * Fetches data from the specified API endpoint and updates the state with the fetched data.
+     *
+     * @return {Promise<void>} - A promise that resolves when the data is fetched and the state is updated.
+     */
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://api.weekday.technology/adhoc/getSampleJdJSON",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            limit: limit,
+            offset: offset,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      // Filter out entries where any property is null
+      const filteredData = data.jdList.filter(
+        (item) => !Object.values(item).includes(null)
+      );
+
+      setApiData((prevData) => [...prevData, ...filteredData]);
+    } catch (e) {
+      console.log("Error on Fetching data", e);
+    }
+  };
+
+
+ // Adding scroll event and remove on unmount
+useEffect(() => {
     window.addEventListener("scroll", infiniteScroll);
     return () => window.removeEventListener('scroll', infiniteScroll);
 
   }, []);
 
-      // fetching the data if offset changes
-    useEffect(() => {
-      fetchData();
-    }, [offset]);
-    
+//   fetching the data if offset changes
+  useEffect(() => {
+    fetchData();
+  }, [offset]);
+
+
+//   reducing loading time by increasing page size if filtered data length is same as previous length
+//  if filtered data length is less than 300 then set loading to false and set limit to 10 (default)
+  useEffect(() => {
+    if(preLength > 300){
+        setLoading(false)
+        setLimit(10)
+      
+    }
+    else if((preLength === filteredData.length && preLength !== 0) || filteredData.length ===0){
+
+        setLimit(prev => prev+10)
+        setOffset(prev => prev+1)
+
+    }
+    else{
+        setPrevLength(filteredData.length)
+        setLoading(false)
+    }
+
+},[filteredData]);
 
   const experienceOptions = [
-    {value:'1', label:'1'},
-    {value:'2', label:'2'},
-    {value:'3', label:'3'},
-    {value:'4', label:'4'},
-    {value:'5', label:'5'},
-    {value:'6', label:'6'},
-    {value:'7', label:'7'},
-    {value:'8', label:'8'},
-    {value:'9', label:'9'},
-    {value:'10', label:'10'},
-
-  ]
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+    { value: "5", label: "5" },
+    { value: "6", label: "6" },
+    { value: "7", label: "7" },
+    { value: "8", label: "8" },
+    { value: "9", label: "9" },
+    { value: "10", label: "10" },
+  ];
 
   const remote = [
     { value: "Remote", label: "remote" },
@@ -111,7 +145,6 @@ const Search = () => {
     { value: "Kolkata", label: "kolkata" },
   ];
 
-
   const basePay = [
     { value: "0", label: "0L" },
     { value: "10", label: "10L" },
@@ -122,7 +155,6 @@ const Search = () => {
     { value: "60", label: "60L" },
     { value: "70", label: "70L" },
   ];
-
 
   const techRoles = [
     { value: "android", label: "Android" },
@@ -137,6 +169,7 @@ const Search = () => {
   return (
     <>
       <div className="searchContainer">
+
         {/* Experience select */}
         <Select
           options={experienceOptions}
@@ -184,7 +217,7 @@ const Search = () => {
       ):null}
 
       {/* card container */}
-      <Card data={data } />
+      <Card data={filteredData } />
     </>
   );
 };
